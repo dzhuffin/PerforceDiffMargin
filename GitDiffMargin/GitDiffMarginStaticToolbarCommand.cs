590bb9a39fa9d886fa366ece1f5d635eae59df9a
wkoder <w-koder@mail.ru>
2020-07-07 20:49:37 +0300

    All code about Changelist Combo box has been removed. New concept with 5 buttons has been done. For now, log-in button is not added because it can be easily introduced in future.

--- GitDiffMargin/GitDiffMarginStaticToolbarCommand.cs
+++ GitDiffMargin/GitDiffMarginStaticToolbarCommand.cs
@@ -5,8 +5,11 @@
     [Guid(GitDiffMarginCommandHandler.GitDiffMarginStaticToolbarCommandSet)]
     public enum GitDiffMarginStaticToolbarCommand
     {
-        ChangelistCombo = 0,
-        ChangelistComboGetList = 1,
+        Refresh = 0,
+        Disconnect = 1,
+        SetP4Port = 2,
+        SetUser = 3,
+        SetWorkspace = 4,
 
         GitDiffStaticToolbar = 100,
         GitDiffStaticToolbarGroup = 150,
